cbfd1a2afb8e617b78ec6ab38176d4706a6bb2d1
fundale <69996669+fundale@users.noreply.github.com>
2022-08-08 22:38:37 -0500

    Changed URL

--- popup/popup.js
+++ popup/popup.js
@@ -9,7 +9,7 @@ chrome.tabs.query({
         if (url.includes("youtube.com/watch?v=")) {
 
             let notTube = url.split("&")[0];
-            let NexTo = `https://t-ne.x0.to/?url=${notTube}`;
+            let NexTo = `https://nextnex.com/?url=${notTube}`;
             navigator.clipboard.writeText(NexTo);
 
         }
